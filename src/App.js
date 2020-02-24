780f79240625340aa92566296480bd286cd95e33
dptole <danillo.paiva.toledo@gmail.com>
2020-02-24 05:24:55 -0300

    - Initial commit.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Clicks from './hooks/clicks'
+
+const App = () => {
+  return (
+    <div className="container">
+      <Clicks />
+    </div>
+  )
+}
+
+export default App
