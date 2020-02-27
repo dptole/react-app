1ed5c415b403f60dac043ab0930776bd57f2083e
dptole <danillo.paiva.toledo@gmail.com>
2020-02-27 10:07:17 -0300

    - Fix routes /clicks/ and /imdb/.

--- src/assets/utils/const.jsx
+++ src/assets/utils/const.jsx
@@ -3,8 +3,8 @@ const root_route = '/react-app'
 
 export const ROUTES = {
   ROOT: root_route,
-  IMDB: root_route + '/clicks/',
-  CLICKS: root_route + '/imdb/',
+  IMDB: root_route + '/imdb/',
+  CLICKS: root_route + '/clicks/',
   MAL: root_route + '/mal/',
   PROGQ: root_route + '/progq/',
 }
